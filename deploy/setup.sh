b6117356773ad8a5745be99f6ce1003efc4109ea
novaardiansyah <novaardiansyah78@gmail.com>
2025-12-27 19:56:43 +0700

    feat: Add deployment setup script and Supervisor configuration for the Go API.

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
+echo "--> Fix permission..."
+
+# Set owner
+sudo chown -R www:www . 2>/dev/null
+
+# Default perms
+sudo find . -type d -exec chmod 755 {} \; 2>/dev/null
+sudo find . -type f -exec chmod 644 {} \; 2>/dev/null
+
+# Sensitive files
+sudo chmod 600 .env .env.local .env.production .well-known .git 2>/dev/null
+sudo chmod -R 600 ./deploy/bash 2>/dev/null
+
 echo "--> Creating log files..."
 rm -rf logs/golang.log logs/golang-error.log
+mkdir -p logs
 touch logs/golang.log logs/golang-error.log
+
+sudo chmod -R 775 logs 2>/dev/null
 
 echo "--> Binary File..."
 sudo chmod +x deploy/bin/api
@@ -15,19 +31,3 @@ sudo supervisorctl update
 
 echo "--> Supervisor restart..."
 sudo supervisorctl restart golang-api_novadev_myid
-
-echo "--> Fix permission..."
-
-# Set owner
-sudo chown -R www:www . 2>/dev/null
-
-# Default perms
-sudo find . -type d -exec chmod 755 {} \; 2>/dev/null
-sudo find . -type f -exec chmod 644 {} \; 2>/dev/null
-
-# Writable directories
-sudo chmod -R 775 logs 2>/dev/null
-
-# Sensitive files
-sudo chmod 600 .env .env.local .env.production .well-known .git 2>/dev/null
-sudo chmod -R 600 ./deploy/bash 2>/dev/null
